794fc4b4f6ce10c965d8992f0cd9bf59eaf711bb
James Humphreys <9253831+jnahumphreys@users.noreply.github.com>
2025-07-31 19:01:51 +0000

    add tests for specifications component

--- apps/frontend/app/product/components/specifications/specifications.stories.tsx
+++ apps/frontend/app/product/components/specifications/specifications.stories.tsx
@@ -22,4 +22,30 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  play: async ({ canvas }) => {
+    const specificationsRegion = canvas.getByRole("region", {
+      name: "Specifications",
+    });
+
+    await expect(specificationsRegion).toBeInTheDocument();
+
+    const regionQueries = within(specificationsRegion);
+    const heading = regionQueries.getByRole("heading", { level: 2 });
+    await expect(heading).toHaveTextContent("Specifications");
+
+    const terms = regionQueries.getAllByRole("term");
+    await expect(terms).toHaveLength(5);
+
+    const definitions = regionQueries.getAllByRole("definition");
+    await expect(definitions).toHaveLength(5);
+
+    await expect(regionQueries.getByText("Brand")).toBeInTheDocument();
+    await expect(regionQueries.getByText("Phillips")).toBeInTheDocument();
+
+    await expect(
+      regionQueries.getByText("Item weight (g)"),
+    ).toBeInTheDocument();
+    await expect(regionQueries.getByText("77")).toBeInTheDocument();
+  },
+};
