cbc121bee4b4f1630d71d2789a48da648758b3f8
James Humphreys <9253831+jnahumphreys@users.noreply.github.com>
2025-07-31 13:37:49 +0000

    remove mdx type stories from config

--- apps/frontend/.storybook/main.ts
+++ apps/frontend/.storybook/main.ts
@@ -10,7 +10,7 @@ function getAbsolutePath(value: string): any {
   return dirname(require.resolve(join(value, "package.json")));
 }
 const config: StorybookConfig = {
-  stories: ["../**/*.mdx", "../**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     getAbsolutePath("@chromatic-com/storybook"),
     getAbsolutePath("@storybook/addon-docs"),
